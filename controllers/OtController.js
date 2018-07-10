3099fc6bb7e1ae3c5c5db33c02b9686e4f55ec36
Patricio <patovaldivia4@gmail.com>
2018-07-10 17:58:48 -0400

    fix json response Ots

--- controllers/OtController.js
+++ controllers/OtController.js
@@ -10,7 +10,7 @@ const getOt = async function(req, res){
     if (err) return ReE(res, err, 422);
     if(!ots) return ReE(res, "OT no encontrada: "+numero_ot);
 
-    return ReS(res, {ots: ots});
+    return ReS(res, {numero_ot: ots.numero_ot, trabajo:ots.trabajo, id_se:ots.SubestacionId});
 
 }
 module.exports.getOt = getOt;
