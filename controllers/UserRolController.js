32f5723c8cd1fe5ba56c3c5a899de03f2218299d
Patricio <patovaldivia4@gmail.com>
2018-07-27 15:31:50 -0400

    avance userRolController

--- controllers/UserRolController.js
+++ controllers/UserRolController.js
@@ -1,5 +1,23 @@
 const UserRols = require('../models').UserRols;
 
+const create = async function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    const body = req.body;
+
+    if(!body.descripcion){
+        return ReE(res, 'Por favor, nombre de Rol');
+    } 
+    else{
+        let err, rol;
+
+        [err, rol] = await to(Rol.create(body));
+
+        if(err) return ReE(res, err, 422);
+        return ReS(res, {message:'Rol creado satisfactoriamente'}, 201);
+    }
+}
+
+module.exports.create = create;
 //get all for a unique ID
 const getRolesForUser = async function(id){
     let err,roles;
