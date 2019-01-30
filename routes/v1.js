3c3d832510c2236c8044e7ceaa0f761a76fa8294
Coronado <jose_182.osorno@hotmail.com>
2019-01-30 12:24:58 -0300

    Update v1.js

--- routes/v1.js
+++ routes/v1.js
@@ -13,6 +13,7 @@ const OtController = require('./../controllers/OtController');
 const EquipoController = require('./../controllers/EquipoController');
 const ComponenteController = require('./../controllers/ComponenteController');
 const RegistroController = require('./../controllers/RegistroController');
+const ImgtermController = require('./../controllers/Img_termController');
 
 require('./../middleware/passport')(passport)
 /* GET home page. */
