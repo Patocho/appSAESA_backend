ad047b1e2b2019ffc12a71b300dd8f01ad7471d7
Patocho <patovaldivia4@gmail.com>
2019-02-01 12:28:49 -0300

    Update 20180812054541-demo-operacion.js

--- seeders/20180812054541-demo-operacion.js
+++ seeders/20180812054541-demo-operacion.js
@@ -8,7 +8,9 @@ module.exports = {
       fechahora_inicio: '2018-08-12T05:51',
       fechahora_fin: '2018-08-12T10:51',
       UserId: 1,
-      OtId: 1
+      OtId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
     },
     ],{});
   },
