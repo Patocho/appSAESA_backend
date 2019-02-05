fb72f9bce1c9e1875195eb06db47bf4a56b2e55c
Patocho <patovaldivia4@gmail.com>
2019-02-05 10:14:11 -0300

    Update SubestacionController.js

--- controllers/SubestacionController.js
+++ controllers/SubestacionController.js
@@ -3,7 +3,20 @@ const Operacion = require('../models').Operacion;
 const Ot =require('../models').Ot;
 const Trampa = require('../models').Trampa;
 const Sequelize = require('../node_modules/sequelize');
-const sequelize = new Sequelize('testsaesa', 'root', 'patoxox132100');
+const sequelize = new Sequelize('testsaesa', 'root', 'patoxox132100', {
+  host: '127.0.0.1',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+  operatorsAliases: false
+});
 const Op = Sequelize.Op;
 
 
