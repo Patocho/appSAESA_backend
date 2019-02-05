013f3bec40797da3ff5a922923b92d1e79de3242
Patocho <patovaldivia4@gmail.com>
2019-02-05 10:16:12 -0300

    Update SubestacionController.js

--- controllers/SubestacionController.js
+++ controllers/SubestacionController.js
@@ -85,7 +85,7 @@ const verDatos = async function(req, res){
 
     }));*/
 
-    sequelize.query('SELECT * FROM Ots', ots).then(function(ots){
+    sequelize.query("SELECT `Ot`.`id`, `Ot`.`numero_ot`, `Ot`.`fecha_ot`, `Ot`.`trabajo`, `Ot`.`createdAt`, `Ot`.`updatedAt`, `Ot`.`deletedAt`, `Ot`.`SubestacionId` FROM `Ots` AS `Ot` INNER JOIN `Operacions` AS `Operacions` ON `Ot`.`id` != `Operacions`.`OtId` AND (`Operacions`.`deletedAt` > '2019-02-04 19:38:26' OR `Operacions`.`deletedAt` IS NULL) WHERE ((`Ot`.`deletedAt` > '2019-02-04 19:38:26' OR `Ot`.`deletedAt` IS NULL) AND `Ot`.`SubestacionId` = '1')", ots).then(function(ots){
         console.log(ots);
     });
 
