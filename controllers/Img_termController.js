47a596e5349abc14e35e69bf9fb49bd4ff20d72a
Coronado <jose_182.osorno@hotmail.com>
2019-03-28 12:30:42 -0300

    Update Img_termController.js

--- controllers/Img_termController.js
+++ controllers/Img_termController.js
@@ -15,7 +15,7 @@ const getImage = async function(req, res){
     let image_json = [];
     for (let i in image) {
         let imagen = image[i];
-        let image_info = imagen.toWeb();
+        let image_info = imagen.toJSON();
 		
 		image_json.push({id:image_info.id, nombre:image_info.nombre, recurso:image_info.recurso, tipo:image_info.tipo, situacion:image_info.situacion, id_comp:image_info.compId, id_op:image_info.operacionId});       
     }    
