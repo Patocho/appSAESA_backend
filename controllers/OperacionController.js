6e522df053c47d1c85d7cdee68262e106e5ad806
Patocho <patovaldivia4@gmail.com>
2019-03-31 03:20:06 -0300

    Update OperacionController.js

--- controllers/OperacionController.js
+++ controllers/OperacionController.js
@@ -27,7 +27,27 @@ const ObtenerParaSubestacion = async function(req, res){
     }));
     if(err) return ReE(res, err, 422);
 
-    return ReS(res,{operaciones:operaciones}, 201);
+    let operaciones_json = [];
+    for (let i in operaciones) {
+        let operacion = operaciones[i];
+        //let alertas_info = alerta.toJSON();
+        let operaciones_info = {
+            id:operacion.id,
+            pt_operacion:operacion.pt_operacion,
+            fechahora_inicio:operacion.fechahora_inicio,
+            fechahora_fin:operacion.fechahora_fin,
+            obs_operacion:operacion.obs_operacion,
+            tr_ret:operacion.tr_ret,
+            tr_inst:operacion.tr_inst,
+            numero_ot:operacion.Ot.numero_ot,
+            cod_se:operacion.Ot.Subestacion.cod_se,
+            nombre_se:operacion.Ot.Subestacion.nombre_se,
+        }
+
+        operaciones_json.push(operaciones_info);
+    }
+
+    return ReS(res,{operaciones:operaciones_json}, 201);
 }
 
 module.exports.ObtenerParaSubestacion = ObtenerParaSubestacion;
