a87bb69b8db1c7ace5cd3d83f9d69ec6afa19100
Coronado <jose_182.osorno@hotmail.com>
2019-04-08 14:35:52 -0400

    Update Img_termController.js

--- controllers/Img_termController.js
+++ controllers/Img_termController.js
@@ -5,21 +5,16 @@ const Operacion = require('../models').Operacion;
 const getImage = async function(req, res){
     res.setHeader('Content-Type', 'application/json');
     let err, image;
-    nombre = req.params.nombre;
-    [err, image] = await to(Img_term.findAll({
+    const body = req.body;
+
+    nombre = body.nombre;
+    [err, image] = await to(Img_term.findOne({
         //attributes: ['id'],
         where:{nombre: nombre}}));
     //console.log(image);
     if (err) return ReE(res, err, 422);
 
-    let image_json = [];
-    for (let i in image) {
-        let imagen = image[i];
-        let image_info = imagen.toJSON();
-		
-		image_json.push({id:image_info.id, nombre:image_info.nombre, recurso:image_info.recurso, tipo:image_info.tipo, situacion:image_info.situacion, id_comp:image_info.compId, id_op:image_info.operacionId});       
-    }    
-    return ReS(res, {image: image_json});
+    return ReS(res, {id: image.id, nombre: image.nombre});
 
 }
 module.exports.getImage = getImage;
