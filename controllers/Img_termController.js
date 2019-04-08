da7b61620eb7d301c804cc459a655578e28ef420
Patocho <patovaldivia4@gmail.com>
2019-04-08 15:41:51 -0400

    Update Img_termController.js

--- controllers/Img_termController.js
+++ controllers/Img_termController.js
@@ -6,15 +6,11 @@ const getImage = async function(req, res){
     res.setHeader('Content-Type', 'application/json');
     let err, image;
     const body = req.body;
-    console.log("###############################################");
-    console.log(body);
     nombre = body.nombre;
     [err, image] = await to(Img_term.findOne({
         //attributes: ['id'],
         where:{nombre: nombre}}));
-    //console.log(image);
     if (err) return ReE(res, err, 422);
-    //console.log(image);
     return ReS(res, {id: image.id, nombre: image.nombre});
 
 }
