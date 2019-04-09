87c30582f76fb33199f3f0e7f0b532b50e7c53b9
Patocho <patovaldivia4@gmail.com>
2019-04-09 14:47:02 -0400

    Update 20180507051410-demo-Ot.js

--- seeders/20180507051410-demo-Ot.js
+++ seeders/20180507051410-demo-Ot.js
@@ -3,8 +3,8 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Ots',[
-    {
-     /* numero_ot: 1001,
+    /*{
+      numero_ot: 1001,
       fecha_ot: '2018-04-15 15:19:06',
       trabajo: 'Control de Plagas',
       SubestacionId: 1,
