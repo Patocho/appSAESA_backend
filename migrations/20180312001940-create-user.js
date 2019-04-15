7dd9295fa09790e0bf38dc4729df6828d521ebdb
Patocho <patovaldivia4@gmail.com>
2019-04-15 14:55:33 -0400

    Update 20180312001940-create-user.js

--- migrations/20180312001940-create-user.js
+++ migrations/20180312001940-create-user.js
@@ -13,6 +13,11 @@ module.exports = {
           allowNull: false,
           unique: true
       },
+      password : {
+          type: DataTypes.STRING, 
+          allowNull: false, 
+          unique: false
+      },
       nombre: {
           type: Sequelize.STRING,
           allowNull: false,
