fb810bc9071374b41af36e4f5e2e6680e7e4ad95
Patocho <patovaldivia4@gmail.com>
2019-05-13 09:55:48 -0400

    Update 20180507051410-demo-Ot.js

--- seeders/20180507051410-demo-Ot.js
+++ seeders/20180507051410-demo-Ot.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Ots',[] ,{});
+    return null;
   },
 
   down: (queryInterface, Sequelize) => {
-      return queryInterface.bulkDelete('Ots', [], {});
+      return null;
   }
 };
