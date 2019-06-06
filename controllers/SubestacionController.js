f6ffdc8646f3bf45ace59f77c3ff625f6808ba9a
Patocho <patovaldivia4@gmail.com>
2019-06-06 11:52:18 -0400

    Update SubestacionController.js

--- controllers/SubestacionController.js
+++ controllers/SubestacionController.js
@@ -181,7 +181,6 @@ const crearNuevaSE = async function(req, res){
         cod_se : cod_se,
         nombre_se: nombre_se
     }
-    console.log(sube);
     
     let subestacion
     if (sube == null) {
