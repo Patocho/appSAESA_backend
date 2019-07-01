d5625e31e85124e765e0fc9d57c59a0e78af0fb6
Patocho <patovaldivia4@gmail.com>
2019-07-01 14:19:19 -0400

    Update 20180706204940-demo-userrol.js

--- seeders/20180706204940-demo-userrol.js
+++ seeders/20180706204940-demo-userrol.js
@@ -4,32 +4,16 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('UserRols',[
     {
+      createdAt: new Date(),
+      updatedAt: new Date(),
       RolId : 1,
       UserId: 1,
-createdAt: new Date(),
-updatedAt: new Date()
-
     },
     {
-      RolId : 2,
-      UserId: 1,
-createdAt: new Date(),
-updatedAt: new Date()
-
-    },
-    {
+      createdAt: new Date(),
+      updatedAt: new Date(),
       RolId : 1,
       UserId: 2,
-createdAt: new Date(),
-updatedAt: new Date()
-
-    },
-    {
-      RolId : 3,
-      UserId: 2,
-createdAt: new Date(),
-updatedAt: new Date()
-
     },
     ],{});
   },
